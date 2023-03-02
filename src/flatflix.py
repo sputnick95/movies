3744385bab7fb87c497136e70cb438a4c48c9837
zoe petropoulou <zoepetropoulou@zoes-MacBook-Air-3.local>
2023-03-02 10:40:52 -0500

    done with challenge

--- src/flatflix.py
+++ src/flatflix.py
@@ -1,52 +1,60 @@
 class Movie:
     def __init__(self, title):
-        pass
-
+        self.title = title
+        self.reviews = []
     def get_title(self):
-        pass
+        return self.title.title()
 
     def get_reviews(self):
-        pass
+        return self.reviews
 
     def get_viewers(self):
-        pass
+        return [review.viewer for review in self.reviews]
 
     def average_rating(self):
-        pass
+        return sum([review.rating for review in self.reviews])/len(self.reviews)
 
-    def highest_rating(self):
-        pass
-
+    def highest_review(self):
+        highest_review = self.reviews[0]
+        for review in self.reviews:
+            if review.rating > highest_review.rating:
+                highest_review = review
+        return highest_review
 
 class Viewer:
     def __init__(self, username):
-        pass
+        self.username = username
+        self.reviews = []
 
     def get_username(self):
-        pass
+        return self.username
 
     def get_reviews(self):
-        pass
+        return self.reviews
 
     def get_movies(self):
-        pass
+        return [review.movie for review in self.reviews]
 
     def has_reviewed(self, movie):
-        pass
+        x = [review.movie for review in self.reviews]
+        return movie in x
 
     def rate_movie(self, movie, rating):
-        pass
+        review = Review(self, movie, rating)
+        self.reviews.append(review)
 
 
 class Review:
     def __init__(self, viewer, movie, rating):
-        pass
+        self.viewer = viewer
+        self.movie = movie
+        self.rating = rating
 
     def get_rating(self):
-        pass
+        return self.rating
 
     def get_viewer(self):
-        pass
+        return self.viewer
 
     def get_movie(self):
-        pass
+        return self.movie
